c4f01fc66ac2806835b78e11018d89d6030ae982
Diegodmh <dmh-95@hotmail.com>
2018-03-01 20:34:00 -0600

    practica6

--- practicas/practica06/main.c
+++ practicas/practica06/main.c
@@ -2,10 +2,8 @@
 #include <stdio.h>
 #include "hash.h"
 
-/* Add function signatures here */
 
 int main(int argc, char **argv) {
-  /* Start your code here */
   printf("Hello from main\n");
   return 0;
 }
